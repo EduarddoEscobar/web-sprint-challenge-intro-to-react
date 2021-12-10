1c8d8cab1f131751ebe157059fa00ea203ebeb5d
Eduardo Escobar <eduarddo.escobar@gmail.com>
2021-12-10 00:41:24 -0500

    Now lists all character without styling too much

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
 
+const CharactersDiv = styled.div`
+`
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characters, setCharacters] = useState(null);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people').then(resp => {
+      console.log(resp.data);
+      setCharacters(resp.data);
+    }).catch(error => console.error(error));
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <CharactersDiv>
+        {characters && 
+        characters.map((character, ind) =><Character character={character} key={ind}/>)
+        }
+      </CharactersDiv>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+import React, {useState, useEffect} from "react";
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    margin: auto;
+    display: flex;
+    width: 40%;
+    justify-content: space-between;
+    font-size: 1.4rem;
+`
+
+export default function Character(props){
+    const { character } = props;
+
+    const [open, setOpen] = useState(false);
+
+    function toggleOpen(){
+        setOpen(!open);
+    }
+
+    return(
+        <StyledDiv>
+
+            {character.name}
+            <button onClick={toggleOpen}>{open ? '🔺' : '🔻'}</button>
+        </StyledDiv>
+    )
+}
